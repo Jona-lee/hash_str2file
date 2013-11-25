8e21a03ea508715d0c33aa3c735165dde519f4cb
Janathan <javenly@gmail.com>
2013-11-25 17:47:45 +0800

    remove unuseful comment

--- src/dlog.c
+++ src/dlog.c
@@ -2,7 +2,6 @@
  *
  *  Copyright (c) 2013 javenly@gmail.com
  * 
- *  Mostly socket code based on original code from apue.2e
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
